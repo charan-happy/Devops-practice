7f27b104d1789189a0c71116838132abd61a0c55
Nagacharan <nagacharan4286@gmail.com>
2024-07-30 03:26:24 +0530

    terraform update

---
+++ Terraform/conditions/variables.tf
@@ -0,0 +1,19 @@
+variable "image_id" {
+    type = string
+    default = "ami-090252cbe067a9e58"
+    description = "RHEL-9 AMI ID" # optional
+}
+
+variable "instance_type" {
+    default = "t3.micro"
+    type = string
+}
+
+variable "tags" {
+    default = {
+        Project = "Expense"
+        Environment = "Dev"
+        Module = "DB"
+        Name = "DB"
+    }
+}
